c75768ed33be391fbac3759299eaea81ed0fa186
Ousman Touray <otouray14@gmail.com>
2023-03-25 19:14:15 -0400

    done

--- src/components/Stock.js
+++ src/components/Stock.js
@@ -1,23 +1,33 @@
 import data from '../data'
-import { useParams } from 'react-router-dom';
+import { useParams, useEffect, useState } from 'react-router-dom';
+
 function Stock(props){
     const params = useParams();
     console.log(params.symbol);
     console.log(props);
-        if(params.symbol === 'AAPL'){
-            console.log(data);
-            return <div> 
-                <h1>hello</h1>
-
-            </div>
-        }
     // switch(props){
     //     case 'AAPL':
     //         return 
     // }
     console.log(params);
     return(
-        <h2></h2>
+        data.map((stock) => {
+            if(stock.symbol == params.symbol){
+                console.log(stock);
+                return(
+                    <div>
+                    <h2><strong>Name:</strong>{stock.name}</h2>
+                    <p><strong>Symbol:</strong>{stock.symbol}</p>
+                    <p><strong>High:</strong>{stock.high}</p>
+                    <p><strong>Change:</strong>{stock.change}</p>
+                    <p><strong>Low:</strong>{stock.low}</p>
+                    <p><strong>Last Price:</strong>{stock.lastPrice}</p>
+                    <p><strong>Open:</strong>{stock.open}</p>
+                </div>
+                    )
+                
+            }
+        })
         
     )
 }
